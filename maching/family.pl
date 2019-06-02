09044843d14870005c40cee48a62e82a7044d33c
Takeshi <g.t.n.ramirez@gmail.com>
2019-06-02 00:04:19 -0500

    reinicio

--- maching/family.pl
+++ maching/family.pl
@@ -1,5 +1,3 @@
-
-
 child_of(joe, ralf).
 child_of(mary, joe).
 child_of(steve, joe).
@@ -8,7 +6,3 @@ descendent_of(X, Y) :-
 descendent_of(X, Y) :-
  child_of(Z, Y),
  descendent_of(X, Z). 
-
-
-
-
